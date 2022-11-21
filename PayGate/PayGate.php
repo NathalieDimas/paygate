f1c87ce6fb005b09aaa5a00aad4530a90ba214dc
NathalieDimas <102619101+NathalieDimas@users.noreply.github.com>
2022-11-21 12:08:28 -0600

    Add files via upload

---
+++ PayGate/PayGate.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+Plugin Name: raiola
+Plugin URI: https://github.com/NathalieDimas/paygate
+Description: Plugin de ejemplo del post de como crear un plugin en WordPress
+Version: 0.1
+Author: Nathalie Dimas
+Author URI: https://github.com/NathalieDimas/paygate
+License: GPL
+*/
+?>
